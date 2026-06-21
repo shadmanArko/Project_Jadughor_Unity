80dbd5331d5ac0bb1c65aac3029065d748c4136a
Samiul Islam <samiulislamshad@gmail.com>
2026-06-21 23:39:04 +0600

    sword uses overlapcircle to detect idamageables

--- Assets/Systems/MineSystem/ToolbarSystem/Handler/WeaponItemActionHandler.cs
+++ Assets/Systems/MineSystem/ToolbarSystem/Handler/WeaponItemActionHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Systems.MineSystem.InventorySystem.Model;
 using Systems.MineSystem.Damage;
+using Systems.MineSystem.Mine.View;
 using Systems.MineSystem.MinePlayerSystem.Scriptable;
 using Systems.MineSystem.MinePlayerSystem.Service;
 using Systems.MineSystem.MinePlayerSystem.View;
@@ -19,6 +20,8 @@ namespace Systems.MineSystem.ToolbarSystem.Handler
         private readonly RuntimeDataScriptable _runtime;
         private readonly ItemActionProfileCatalog _catalog;
         private readonly PlayerView _player;
+        private readonly MineView _mineView;
+        private readonly List<Collider2D> _hitResults = new(32);
 
         public override ItemActionKind ActionKind => ItemActionKind.Weapon;
 
@@ -27,6 +30,7 @@ namespace Systems.MineSystem.ToolbarSystem.Handler
             RuntimeDataScriptable runtime,
             ItemActionProfileCatalog catalog,
             PlayerView player,
+            MineView mineView,
             IPlayerItemAnimationService animation,
             IToolbarNavigationLock navigationLock)
             : base(animation, navigationLock)
@@ -35,6 +39,7 @@ namespace Systems.MineSystem.ToolbarSystem.Handler
             _runtime = runtime;
             _catalog = catalog;
             _player = player;
+            _mineView = mineView;
         }
 
         protected override bool TryPrepareAction(
@@ -71,13 +76,31 @@ namespace Systems.MineSystem.ToolbarSystem.Handler
             WeaponActionProfile profile,
             ItemActionTarget target)
         {
-            var colliders = Physics2D.OverlapCircleAll(
-                target.WorldPosition,
-                profile.HitRadius,
-                profile.TargetLayers);
-            var damaged = new HashSet<IDamageable>();
-            foreach (var collider in colliders)
+            var direction = ToVector(target.Direction);
+            var cellSize = GetDirectionalCellSize(direction);
+            var origin = (Vector2)_player.PlayerCollider.bounds.center;
+            var center =
+                origin + direction * (profile.Range * cellSize);
+            var radius = profile.HitRadiusCells * cellSize;
+            var filter = new ContactFilter2D
             {
+                useLayerMask = true,
+                layerMask = profile.TargetLayers,
+                useTriggers = true
+            };
+            _hitResults.Clear();
+            Physics2D.OverlapCircle(
+                center,
+                radius,
+                filter,
+                _hitResults);
+            var damaged = new HashSet<IDamageable>();
+            for (var index = 0; index < _hitResults.Count; index++)
+            {
+                var collider = _hitResults[index];
+                if (collider == null)
+                    continue;
+
                 var behaviours = collider.GetComponentsInParent<MonoBehaviour>();
                 foreach (var behaviour in behaviours)
                 {
@@ -89,6 +112,32 @@ namespace Systems.MineSystem.ToolbarSystem.Handler
                     }
                 }
             }
+        }
+
+        private float GetDirectionalCellSize(Vector2 direction)
+        {
+            var grid = _mineView.grid;
+            var origin = grid.CellToWorld(Vector3Int.zero);
+            var adjacent = grid.CellToWorld(
+                direction.x != 0f
+                    ? Vector3Int.right
+                    : Vector3Int.up);
+            return Mathf.Max(
+                0.0001f,
+                direction.x != 0f
+                    ? Mathf.Abs(adjacent.x - origin.x)
+                    : Mathf.Abs(adjacent.y - origin.y));
+        }
+
+        private static Vector2 ToVector(CardinalDirection direction)
+        {
+            return direction switch
+            {
+                CardinalDirection.Up => Vector2.up,
+                CardinalDirection.Down => Vector2.down,
+                CardinalDirection.Left => Vector2.left,
+                _ => Vector2.right
+            };
         }
     }
 }
--- Assets/Systems/MineSystem/ToolbarSystem/Profile/WeaponActionProfile.cs
+++ Assets/Systems/MineSystem/ToolbarSystem/Profile/WeaponActionProfile.cs
@@ -1,5 +1,6 @@
 using Systems.MineSystem.ToolbarSystem.Enum;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Systems.MineSystem.ToolbarSystem.Profile
 {
@@ -8,13 +9,16 @@ namespace Systems.MineSystem.ToolbarSystem.Profile
     {
         [Min(0f)]
         [SerializeField] private float damage = 10f;
+        [Tooltip("Radius of the damage circle measured in mine cells.")]
+        [FormerlySerializedAs("hitRadius")]
         [Min(0.01f)]
-        [SerializeField] private float hitRadius = 0.5f;
+        [SerializeField] private float hitRadiusCells = 0.75f;
         [SerializeField] private LayerMask targetLayers = ~0;
 
         public override ItemActionKind ActionKind => ItemActionKind.Weapon;
         public float Damage => Mathf.Max(0f, damage);
-        public float HitRadius => Mathf.Max(0.01f, hitRadius);
+        public float HitRadiusCells =>
+            Mathf.Max(0.01f, hitRadiusCells);
         public LayerMask TargetLayers => targetLayers;
     }
 }
